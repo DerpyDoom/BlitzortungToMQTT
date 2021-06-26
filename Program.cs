df9b44cc9c2691ce9a2019163f4ad1441e3f88b4
DerpyDoom <andre.bartelsmeir@gmail.com>
2021-06-26 04:23:59 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlitzortungToMQTT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
